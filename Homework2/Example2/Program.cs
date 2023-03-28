1c8983dc78ea480655341bd500d9d81fb4ac0bea
Elena <kor-09-06-2008@yandex.ru>
2023-03-28 21:16:07 +0300

    Добавлен Homework2

---
+++ Homework2/Example2/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num100 = num % 10;
+
+int num1000 = (num /10) % 10;
+
+int num10000 = (num /100) % 10;
+
+if (num < 100)
+{
+   Console.WriteLine("третьей цифры нет"); 
+}
+else if (num > 100 && num < 1000)
+ {
+    Console.WriteLine(num100); 
+}
+else if (num > 1000 && num < 10000)
+ {
+    Console.WriteLine(num1000);
+ }
+ else if (num > 10000 && num < 100000)
+ {
+    Console.WriteLine(num10000);
+ }
